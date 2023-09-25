5963adc0a429c911dd516f3ee759e1ce01d8eaca
willl85 <ngowilfried@gmail.com>
2023-09-25 17:18:37 +0200

    Adding the New Meetup Form

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,12 @@
+// our-domain.com/new-meetup
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
+function NewMeetup() {
+  function addMeetupHandler(enteredMeetupData) {
+    console.log(enteredMeetupData);
+  }
+  
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+}
+
+export default NewMeetup;
