ae82cf9dad89a3ba9207f5678c343fc7a2c6d0d3
willl85 <ngowilfried@gmail.com>
2023-09-25 18:10:22 +0200

    Using Programmatic (Imperative) Navigation and Adding Custom Components and Css Modules

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://images.unsplash.com/photo-1556761175-5973dc0f32e7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1932&q=80"
+      title="A First Meetup"
+      address="Some Street 5, Some City"
+      description="The meetup" 
+    />
+  );
+}
+
+export default MeetupDetails;
