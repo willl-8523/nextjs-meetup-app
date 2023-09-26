0272d1fbc504efb3be3855a0f0e22682fa9003f5
willl85 <ngowilfried@gmail.com>
2023-09-26 11:27:13 +0200

    Introducing API Route

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,11 @@
+// /api/new-meetup
+
+function handler(req, res) {
+  if (redirect.method === 'POST') {
+    const data = req.body;
+
+    const { title, image, address, description } = data;
+  }
+}
+
+export default handler;
