3cdece2e033c34ba606cb82a57ec762a387dffb1
willl85 <ngowilfried@gmail.com>
2023-09-26 20:57:14 +0200

    Sending Http Requests To Our API Routes

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,24 @@
+import { MongoClient } from 'mongodb';
 // /api/new-meetup
 
-function handler(req, res) {
-  if (redirect.method === 'POST') {
+async function handler(req, res) {
+  if (req.method === 'POST') {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      'mongodb+srv://symfonypropar:PjR8ebM9vujPViEX@cluster0.55nsvyz.mongodb.net/meetups?retryWrites=true&w=majority'
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
   }
 }
 
